836ccfed727e2ee470e547987e8459660c19245a
Jren56 <106328383+Jren56@users.noreply.github.com>
2024-09-09 19:40:36 +0800

    section 14-15 208

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <template>
+  <main>
   <!-- Introduction -->
   <section class="mb-8 py-20 text-white text-center relative">
     <div
@@ -25,10 +26,10 @@
   <!-- Main Content -->
   <section class="container mx-auto">
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200" 
+        v-icon.right.yellow="'headphones-alt'">
         <span class="card-title">Songs</span>
         <!-- Icon -->
-        <i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>
       </div>
       <!-- Playlist -->
       <ol id="playlist">
@@ -38,6 +39,7 @@
       <!-- .. end Playlist -->
     </div>
   </section>
+</main>
 </template>
 
 <script>
